e9bb9003321b93d474ef614b868ce4c72e4b55b7
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-03 22:51:12 +0300

    feat: add href to Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import styled, { css } from 'styled-components'
-import ButtonStyled from './styles'
+import React from "react";
+import { ButtonStyled } from "./styles";
 
-const Button = ({ type1, type2, value, width }) => {
-  return (
-    <ButtonStyled type1={type1} type2={type2} width={width}>{value}</ButtonStyled>
-  )
-}
+const Button = ({ type1, type2, value, width, href }) => (
+  <ButtonStyled type1={type1} type2={type2} width={width} href={href}>
+    {value}
+  </ButtonStyled>
+);
 
-export default Button
+export default Button;
