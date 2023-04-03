9db3e6c84ae8e04a28d16db05efdadf4c335b215
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-03 22:58:24 +0300

    chore: add nav links to constants

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const navLinks = [
+    {
+        id: 'home',
+        title: 'Home',
+        link: '/'
+    },
+    {
+        id: 'about-us',
+        title: 'About Us',
+        link: '/about'
+    },
+]
