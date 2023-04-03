0ff0e76212280432dd2f35187eb325203402078c
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-03 17:21:06 +0300

    chore: export assets

---
+++ src/assets/index.js
@@ -0,0 +1,59 @@
+import blog_image_1 from './blog/blog_image_1.jpg'
+import blog_image_2 from './blog/blog_image_2.jpg'
+import blog_image_3 from './blog/blog_image_3.jpg'
+import logo_large from './brand/logo_large.svg'
+import logo_small from './brand/logo_small.svg'
+import clients_logo_facebook from './clients/clients_logo_facebook.svg'
+import clients_logo_google from './clients/clients_logo_google.svg'
+import clients_logo_linkedin from './clients/clients_logo_linkedin.svg'
+import clients_logo_youtube from './clients/clients_logo_youtube.svg'
+import close from './icons/close.svg'
+import hamburger from './icons/hamburger.svg'
+import time from './icons/time.svg'
+import logo_facebook from './companies/logo_facebook.svg'
+import logo_google from './companies/logo_google.svg'
+import logo_instagram from './companies/logo_instagram.svg'
+import logo_messenger from './companies/logo_messenger.svg'
+import logo_twitter from './companies/logo_twitter.svg'
+import logo_webflow from './companies/logo_webflow.svg'
+import logo_youtube from './companies/logoyoutubek.svg'
+import socials_logo_facebook from './socials/socials_logo_facebook.svg'
+import socials_logo_instagram from './socials/socials_logo_instagram.svg'
+import socials_logo_twitter from './socials/socials_logo_twitter.svg'
+import testimonial_logo_1 from './testimonials/testimonial_logo_1.svg'
+import testimonial_logo_2 from './testimonials/testimonial_logo_2.svg'
+import testimonial_logo_3 from './testimonials/testimonial_logo_3.svg'
+import testimonial_profile_pic_1 from './testimonials/testimonial_profile_pic_1.jpg'
+import testimonial_profile_pic_2 from './testimonials/testimonial_profile_pic_2.jpg'
+import testimonial_profile_pic_3 from './testimonials/testimonial_profile_pic_3.jpg'
+
+export {
+    blog_image_1,
+    blog_image_2,
+    blog_image_3,
+    logo_large,
+    logo_small,
+    clients_logo_facebook,
+    clients_logo_google,
+    clients_logo_linkedin,
+    clients_logo_youtube,
+    close,
+    hamburger,
+    time,
+    logo_facebook,
+    logo_google,
+    logo_instagram,
+    logo_messenger,
+    logo_twitter,
+    logo_webflow,
+    logo_youtube,
+    socials_logo_facebook,
+    socials_logo_instagram,
+    socials_logo_twitter,
+    testimonial_logo_1,
+    testimonial_logo_2,
+    testimonial_logo_3,
+    testimonial_profile_pic_1,
+    testimonial_profile_pic_2,
+    testimonial_profile_pic_3
+}
