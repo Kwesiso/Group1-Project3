53e48404bed3a77780b6bd598b28fcb794aef873
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-04 15:40:49 +0300

    chore: add clients to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,38 @@
+import {
+  clients_logo_facebook,
+  clients_logo_google,
+  clients_logo_linkedin,
+  clients_logo_youtube,
+} from "../assets";
+
 export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: "about-us",
+    title: "About Us",
+    link: "/about",
+  },
+];
+
+export const clients = [
     {
-        id: 'home',
-        title: 'Home',
-        link: '/'
+        id: '1',
+        logo: clients_logo_facebook
     },
     {
-        id: 'about-us',
-        title: 'About Us',
-        link: '/about'
+        id: '2',
+        logo: clients_logo_google
+    },
+    {
+        id: '3',
+        logo: clients_logo_youtube
+    },
+    {
+        id: '4',
+        logo: clients_logo_linkedin
     },
 ]
