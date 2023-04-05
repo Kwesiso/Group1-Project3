cafc2ca7ab0125ebe13b79860e24ac7a396613e8
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-05 14:29:29 +0300

    chore: add featured companies to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,10 @@ import {
   clients_logo_google,
   clients_logo_linkedin,
   clients_logo_youtube,
+  logo_facebook,
+  logo_twitter,
+  logo_instagram,
+  logo_google
 } from "../assets";
 
 export const navLinks = [
@@ -35,4 +39,31 @@ export const clients = [
         id: '4',
         logo: clients_logo_linkedin
     },
+]
+
+export const featuredCompanies = [
+  {
+    id: '1',
+    logo: logo_facebook,
+    title: 'Facebook',
+    type: 'Social Network'
+},
+{
+    id: '2',
+    logo: logo_twitter,
+    title: 'Twitter',
+    type: 'Social Network'
+},
+{
+    id: '3',
+    logo: logo_instagram,
+    title: 'Instagram',
+    type: 'Social Network'
+},
+{
+    id: '4',
+    logo: logo_google,
+    title: 'Google',
+    type: 'Technology'
+},
 ]
