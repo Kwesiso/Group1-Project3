8baa584a0c4dedb0675e0c2b07c8a1c6c74b9e55
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-05 19:11:58 +0300

    chore: add testimonials to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,13 @@ import {
   logo_facebook,
   logo_twitter,
   logo_instagram,
-  logo_google
+  logo_google,
+  testimonial_logo_1,
+  testimonial_logo_2,
+  testimonial_logo_3,
+  testimonial_profile_pic_1,
+  testimonial_profile_pic_2,
+  testimonial_profile_pic_3,
 } from "../assets";
 
 export const navLinks = [
@@ -23,47 +29,77 @@ export const navLinks = [
 ];
 
 export const clients = [
-    {
-        id: '1',
-        logo: clients_logo_facebook
-    },
-    {
-        id: '2',
-        logo: clients_logo_google
-    },
-    {
-        id: '3',
-        logo: clients_logo_youtube
-    },
-    {
-        id: '4',
-        logo: clients_logo_linkedin
-    },
-]
+  {
+    id: "1",
+    logo: clients_logo_facebook,
+  },
+  {
+    id: "2",
+    logo: clients_logo_google,
+  },
+  {
+    id: "3",
+    logo: clients_logo_youtube,
+  },
+  {
+    id: "4",
+    logo: clients_logo_linkedin,
+  },
+];
 
 export const featuredCompanies = [
   {
-    id: '1',
+    id: "1",
     logo: logo_facebook,
-    title: 'Facebook',
-    type: 'Social Network'
-},
-{
-    id: '2',
+    title: "Facebook",
+    type: "Social Network",
+  },
+  {
+    id: "2",
     logo: logo_twitter,
-    title: 'Twitter',
-    type: 'Social Network'
-},
-{
-    id: '3',
+    title: "Twitter",
+    type: "Social Network",
+  },
+  {
+    id: "3",
     logo: logo_instagram,
-    title: 'Instagram',
-    type: 'Social Network'
-},
-{
-    id: '4',
+    title: "Instagram",
+    type: "Social Network",
+  },
+  {
+    id: "4",
     logo: logo_google,
-    title: 'Google',
-    type: 'Technology'
-},
-]
+    title: "Google",
+    type: "Technology",
+  },
+];
+
+export const testimonials = [
+  {
+    id: "1",
+    testimonial:
+      "We've hired several qualified applicants who report first seeing the position on Jobs, and we'll definitely utilize your services again in future recruiting efforts. It's been great working with you!",
+    logo: testimonial_logo_1,
+    picture: testimonial_profile_pic_1,
+    name: "Sophie Moore",
+    position: "UI Designer at Facebook",
+  },
+  {
+    id: "2",
+    testimonial:
+      "It brings us the highest concentration of quality applicants. It's been great working with you!",
+    logo: testimonial_logo_2,
+    picture: testimonial_profile_pic_2,
+    name: "Mark Norman",
+    position: "SEO Lead at Webflow",
+  },
+  {
+    id: "3",
+    testimonial:
+      "Jobs is a wonderful source for highly educated, experienced professionals looking for meaningful, tech work.",
+    logo: testimonial_logo_3,
+    picture: testimonial_profile_pic_3,
+    name: "Edwin Collins",
+    position: "React Developer at Youtube",
+  },
+];
