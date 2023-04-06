457d14eb968118efe4599ebf455d6bba1e85c0ad
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-06 16:55:34 +0300

    chore: add id to Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ButtonStyled } from "./styles";
 
-const Button = ({ type1, type2, value, width, href, heroButton }) => (
+const Button = ({ type1, type2, value, width, href, heroButton, id }) => (
   <ButtonStyled
     type1={type1}
     type2={type2}
     width={width}
     href={href}
     heroButton={heroButton}
+    id={id}
   >
     {value}
   </ButtonStyled>
