ca587b648b0a0bad8e5f915701c94c4f9afb507e
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-10 22:22:52 +0300

    chore: add css styles to styles file

--- src/pages/JobPostPage/index.jsx
+++ src/pages/JobPostPage/index.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 import { jobDetails } from "../../constants/fetchFromApi";
 import JobCard from "../../components/JobCard";
 import { location, money, time } from "../../assets";
@@ -9,158 +8,7 @@ import CTA from "../../components/CTA";
 import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 import { LoadingSpinner, Span } from "../../globals";
 import { ImSpinner2 } from "react-icons/im";
-
-export const JobsPostPageStyled = styled.main`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: var(--color-neutral-100);
-`;
-
-export const JobPresentation = styled.section`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: var(--space-56) 0 7.5rem;
-  gap: var(--space-32);
-`;
-
-export const JobPresentationContainer = styled.div`
-  width: 100%;
-  padding: 0 var(--space-16);
-  max-width: var(--container-default-max-width);
-  display: flex;
-  flex-direction: column;
-  gap: var(--space-48);
-
-  @media (min-width: 1024px) {
-    flex-direction: row;
-  }
-`;
-
-export const JobCardExtended = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-radius: var(--border-radius-lg);
-  box-shadow: 4px 4px 12px 0 rgba(21, 60, 245, 0.05),
-    0 2px 6px 0 rgba(5, 21, 46, 0.02);
-  background: var(--color-white);
-  padding: var(--space-48);
-  gap: var(--space-32);
-  height: fit-content;
-`;
-
-export const StatsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: var(--space-24);
-  padding-bottom: var(--space-40);
-  border-bottom: 0.1rem solid var(--color-neutral-200);
-
-  @media (min-width: 768px) {
-    gap: var(--space-16);
-  }
-`;
-
-export const Stat = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: var(--space-10);
-`;
-
-export const Divider = styled.div`
-  @media (min-width: 768px) {
-    width: 0.1rem;
-    margin: 0 var(--space-24);
-    background: var(--color-neutral-200);
-  }
-`;
-
-export const Icon = styled.img`
-  width: 1.5rem;
-`;
-
-export const StatText = styled.p`
-  text-transform: uppercase;
-  font-size: 1rem;
-  font-weight: 500;
-  letter-spacing: 2px;
-`;
-
-export const JobDescriptionTitle = styled.h2`
-  font-size: var(--heading-h2-sm);
-  line-height: var(--line-height-h2-sm);
-  width: 100%;
-`;
-
-export const JobDescriptionText = styled.p`
-  max-width: 100%;
-`;
-
-export const AsideContainer = styled.aside`
-  width: 100%;
-  height: fit-content;
-  border-radius: var(--border-radius-lg);
-  box-shadow: 0 7px 8px 0 rgba(30, 54, 170, 0.08);
-  padding: var(--space-36) var(--space-24);
-  background: var(--color-white);
-
-  @media (min-width: 1024px) {
-    position: static;
-    max-width: 30%;
-  }
-`;
-
-export const FormTitle = styled.h3`
-  font-size: var(--heading-h3-sm);
-  margin-bottom: var(--space-24);
-`;
-
-export const ApplyForm = styled.form`
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-  gap: var(--space-36);
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-  }
-`;
-
-export const Asterisk = styled.span`
-  color: red;
-`;
-
-export const ApplyFormInput = styled.input`
-  max-width: 100%;
-`;
-
-export const ApplyFormTextArea = styled.textarea`
-  max-width: 100%;
-`;
-
-export const FormDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  @media (min-width: 768px) {
-    width: 45%;
-  }
-
-  @media (min-width: 1024px) {
-    width: 100%;
-  }
-`;
+import * as J from "./styles";
 
 const JobPostPage = () => {
   const { id } = useParams();
@@ -170,6 +18,8 @@ const JobPostPage = () => {
   const [formSubmitted, setFormSubmitted] = useState(false);
 
   useEffect(() => {
+    window.scroll(0, 0);
+
     jobDetails(id).then((data) => {
       setJobData(data);
       setLoading(false);
@@ -199,42 +49,49 @@ const JobPostPage = () => {
     }
   }, [entries, setObservedNodes]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormSubmitted(true);
+
+    window.scroll(0, 0);
+  };
+
   return (
-    <JobsPostPageStyled>
-      <JobPresentation>
-        <JobPresentationContainer>
+    <J.JobsPostPageStyled>
+      <J.JobPresentation>
+        <J.JobPresentationContainer>
           {/* Display job card */}
-          <JobCardExtended>
+          <J.JobCardExtended>
             {!loading && jobData?.length > 0 ? (
               <>
                 <JobCard {...jobData[0]} inJobPostPage />
-                <StatsContainer>
-                  <Stat>
-                    <Icon src={time} alt="time" />
-                    <StatText>
+                <J.StatsContainer>
+                  <J.Stat>
+                    <J.Icon src={time} alt="time" />
+                    <J.StatText>
                       {jobData[0]?.job_employment_type || "N/A"}
-                    </StatText>
-                  </Stat>
-                  <Divider />
-                  <Stat>
-                    <Icon src={location} alt="time" />
-                    <StatText>
+                    </J.StatText>
+                  </J.Stat>
+                  <J.Divider />
+                  <J.Stat>
+                    <J.Icon src={location} alt="time" />
+                    <J.StatText>
                       {jobData[0]?.job_city || "N/A"},{" "}
                       {jobData[0]?.job_state || "N/A"}
-                    </StatText>
-                  </Stat>
-                  <Divider />
-                  <Stat>
-                    <Icon src={money} alt="time" />
-                    <StatText>
+                    </J.StatText>
+                  </J.Stat>
+                  <J.Divider />
+                  <J.Stat>
+                    <J.Icon src={money} alt="time" />
+                    <J.StatText>
                       ${jobData[0]?.job_min_salary || "N/A"} -{" "}
                       {jobData[0]?.job_max_salary || "N/A"} USD
-                    </StatText>
-                  </Stat>
-                </StatsContainer>
+                    </J.StatText>
+                  </J.Stat>
+                </J.StatsContainer>
 
-                <JobDescriptionTitle>Job Description</JobDescriptionTitle>
-                <JobDescriptionText>
+                <J.JobDescriptionTitle>Job Description</J.JobDescriptionTitle>
+                <J.JobDescriptionText>
                   <pre
                     style={{
                       overflowX: "auto",
@@ -247,136 +104,135 @@ const JobPostPage = () => {
                   >
                     {jobData[0]?.job_description}
                   </pre>
-                </JobDescriptionText>
+                </J.JobDescriptionText>
               </>
             ) : (
               <LoadingSpinner>
                 <ImSpinner2 />
               </LoadingSpinner>
             )}
-          </JobCardExtended>
+          </J.JobCardExtended>
 
-          <AsideContainer id="apply">
+          <J.AsideContainer id="apply">
             {!formSubmitted ? (
               <>
-                <FormTitle>Ready to apply for this job opening?</FormTitle>
-                <ApplyForm
+                <J.FormTitle>Ready to apply for this job opening?</J.FormTitle>
+                <J.ApplyForm
                   onSubmit={(e) => {
-                    e.preventDefault();
-                    setFormSubmitted(true);
+                    handleSubmit(e);
                   }}
                 >
-                  <FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="full-name">
-                      Full Name<Asterisk>*</Asterisk>
+                      Full Name<J.Asterisk>*</J.Asterisk>
                     </label>
-                    <ApplyFormInput
+                    <J.ApplyFormInput
                       type="text"
                       placeholder="Sophie Moore"
                       name="full-name"
                       required={true}
                       maxLength="256"
                     />
-                  </FormDiv>
-                  <FormDiv>
+                  </J.FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="email">
-                      Email<Asterisk>*</Asterisk>
+                      Email<J.Asterisk>*</J.Asterisk>
                     </label>
-                    <ApplyFormInput
+                    <J.ApplyFormInput
                       type="email"
                       placeholder="sophie@email.com"
                       name="email"
                       required={true}
                       maxLength="256"
                     />
-                  </FormDiv>
-                  <FormDiv>
+                  </J.FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="phone-number">
-                      Phone Number<Asterisk>*</Asterisk>
+                      Phone Number<J.Asterisk>*</J.Asterisk>
                     </label>
-                    <ApplyFormInput
+                    <J.ApplyFormInput
                       type="text"
                       placeholder="(123) 456-7890"
                       name="phone-number"
                       required={true}
                       maxLength="256"
                     />
-                  </FormDiv>
-                  <FormDiv>
+                  </J.FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="job-category">
-                      Job Category<Asterisk>*</Asterisk>
+                      Job Category<J.Asterisk>*</J.Asterisk>
                     </label>
-                    <ApplyFormInput
+                    <J.ApplyFormInput
                       type="text"
                       placeholder="Ex.: Development"
                       name="job-category"
                       required={true}
                       maxLength="256"
                     />
-                  </FormDiv>
-                  <FormDiv>
+                  </J.FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="specialization">
-                      Specialization<Asterisk>*</Asterisk>
+                      Specialization<J.Asterisk>*</J.Asterisk>
                     </label>
-                    <ApplyFormInput
+                    <J.ApplyFormInput
                       type="text"
                       placeholder="Ex.: Frontend"
                       name="specialization"
                       required={true}
                       maxLength="256"
                     />
-                  </FormDiv>
-                  <FormDiv>
+                  </J.FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="skills">
-                      Skills<Asterisk>*</Asterisk>
+                      Skills<J.Asterisk>*</J.Asterisk>
                     </label>
-                    <ApplyFormTextArea
+                    <J.ApplyFormTextArea
                       placeholder="Skill 1, Skill 2, Skill 3..."
                       name="skills"
                       required={true}
                       maxLength="5000"
                     />
-                  </FormDiv>
-                  <FormDiv>
+                  </J.FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="resume">
-                      Resume<Asterisk>*</Asterisk>
+                      Resume<J.Asterisk>*</J.Asterisk>
                     </label>
-                    <ApplyFormInput
+                    <J.ApplyFormInput
                       type="text"
                       placeholder="Resume or Portfolio Link"
                       name="resume"
                       required={true}
                       maxLength="256"
                     />
-                  </FormDiv>
-                  <FormDiv>
+                  </J.FormDiv>
+                  <J.FormDiv>
                     <label htmlFor="notes">Notes</label>
-                    <ApplyFormTextArea
+                    <J.ApplyFormTextArea
                       placeholder="If you would like to include any extra note or cover letter, please free to do it here."
                       name="notes"
                       maxLength="5000"
                     />
-                  </FormDiv>
+                  </J.FormDiv>
                   <Button
                     type1="primary"
                     type2="large"
                     value="Apply now"
                     width="100%"
-                    style={{  }}
+                    style={{}}
                   />
-                </ApplyForm>
+                </J.ApplyForm>
               </>
             ) : (
-              <FormTitle>
+              <J.FormTitle>
                 Thank you! Your submission has been <Span>received!</Span>
-              </FormTitle>
+              </J.FormTitle>
             )}
-          </AsideContainer>
-        </JobPresentationContainer>
-      </JobPresentation>
+          </J.AsideContainer>
+        </J.JobPresentationContainer>
+      </J.JobPresentation>
 
       <CTA innerRef={(element) => targets.current.add(element)} />
-    </JobsPostPageStyled>
+    </J.JobsPostPageStyled>
   );
 };
 
