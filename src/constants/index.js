527d3e5963e2225e49309ecda2f5cb096148b961
Raluca Curt <ralucaioanacurt@gmail.com>
2023-04-10 22:03:27 +0300

    chore: update ids in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -38,44 +38,44 @@ export const navLinks = [
 
 export const clients = [
   {
-    id: "1",
+    id: "client-1",
     logo: clients_logo_facebook,
   },
   {
-    id: "2",
+    id: "client-2",
     logo: clients_logo_google,
   },
   {
-    id: "3",
+    id: "client-3",
     logo: clients_logo_youtube,
   },
   {
-    id: "4",
+    id: "client-4",
     logo: clients_logo_linkedin,
   },
 ];
 
 export const featuredCompanies = [
   {
-    id: "1",
+    id: "company-1",
     logo: logo_facebook,
     title: "Facebook",
     type: "Social Network",
   },
   {
-    id: "2",
+    id: "company-2",
     logo: logo_twitter,
     title: "Twitter",
     type: "Social Network",
   },
   {
-    id: "3",
+    id: "company-3",
     logo: logo_instagram,
     title: "Instagram",
     type: "Social Network",
   },
   {
-    id: "4",
+    id: "company-4",
     logo: logo_google,
     title: "Google",
     type: "Technology",
@@ -84,7 +84,7 @@ export const featuredCompanies = [
 
 export const testimonials = [
   {
-    id: "1",
+    id: "testimonial-1",
     testimonial:
       "We've hired several qualified applicants who report first seeing the position on Jobs. It's been great working with you!",
     logo: testimonial_logo_1,
@@ -93,7 +93,7 @@ export const testimonials = [
     position: "UI Designer at Webflow",
   },
   {
-    id: "2",
+    id: "testimonial-2",
     testimonial:
       "It brings us the highest concentration of quality applicants. It's been great working with you!",
     logo: testimonial_logo_2,
@@ -102,7 +102,7 @@ export const testimonials = [
     position: "SEO Lead at Facebook",
   },
   {
-    id: "3",
+    id: "testimonial-3",
     testimonial:
       "Jobs is a wonderful source for highly educated, experienced professionals looking for meaningful, tech work.",
     logo: testimonial_logo_3,
@@ -114,12 +114,12 @@ export const testimonials = [
 
 export const footerLists = [
   {
-    id: "1",
+    id: "Menu",
     title: "Menu",
     links: ["Home", "About Us", "Jobs"],
   },
   {
-    id: "2",
+    id: "Follow Us",
     title: "Follow Us",
     links: ["Facebook", "Twitter", "Instagram"],
     socialLogo: [
@@ -132,17 +132,17 @@ export const footerLists = [
 
 export const footerNav = [
     {
-      id: "home",
+      id: "footer-home",
       title: "Home",
       link: "/",
     },
     {
-      id: "about-us",
+      id: "footer-about-us",
       title: "About Us",
       link: "/about",
     },
     {
-      id: "jobs",
+      id: "footer-jobs",
       title: "Jobs",
       link: "/jobs",
     },
@@ -150,19 +150,19 @@ export const footerNav = [
 
 export const socialNav = [
   {
-    id: '1',
+    id: 'social-1',
     title: 'Facebook',
     icon: socials_logo_facebook,
     link: 'http://facebook.com/'
   },
   {
-    id: '2',
+    id: 'social-2',
     title: 'Twitter',
     icon: socials_logo_twitter,
     link: 'http://twitter.com/'
   },
   {
-    id: '3',
+    id: 'social-3',
     title: 'Instagram',
     icon: socials_logo_instagram,
     link: 'http://instagram.com/'
